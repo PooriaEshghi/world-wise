435fbac3400b1573a6e6ff063fcfe262903dca79
PooriaEshghi <pooriaeshghi@gmail.com>
2024-02-17 13:00:00 +0100

    app layout

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/AppNav"
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <div>AppLayout</div>
+    </div>
+  )
+}
+
+export default AppLayout
