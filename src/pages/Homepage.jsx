f336f2fe2bb2d58d0a01d32ceba3f9a6990054b9
PooriaEshghi <pooriaeshghi@gmail.com>
2024-02-17 12:58:12 +0100

    home page

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,15 +1,24 @@
-import { Link } from "react-router-dom"
-import PageNav from "../components/PageNav"
+import { Link } from "react-router-dom";
+import styles from "./Homepage.module.css";
+import PageNav from "../components/PageNav";
 
-function Homepage() {
+export default function Homepage() {
   return (
-    <div>
-<PageNav />
-    <h1>World Wise</h1>
-    <Link to='/pricing'>Pricing</Link>
-
-    </div>
-  )
+    <main className={styles.homepage}>
+      <PageNav />
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+        <Link to='/app' className="cta" > Start Traching Now</Link>
+      </section>
+    </main>
+  );
 }
-
-export default Homepage
